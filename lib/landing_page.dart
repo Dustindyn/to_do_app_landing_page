25cd93f3eac794b3ee49995a49c33f1040f69cdc
Dustin <dustin.tamalun@gmail.com>
2024-08-18 16:58:10 +0200

    add gradient background

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -10,8 +10,33 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: const Placeholder(),
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: <Color>[
+            Color(0xff414141),
+            Color(0xff383838),
+            Color(0xff303030),
+            Color(0xff272727),
+            Color(0xff1f1f1f),
+            Color(0xff171717),
+            Color(0xff0e0e0e),
+            Color(0xff000000),
+          ],
+          tileMode: TileMode.repeated,
+        ),
+      ),
+      child: MaterialApp(
+        home: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Container(),
+          ),
+        ),
+      ),
     );
   }
 }
